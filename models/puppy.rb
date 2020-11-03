e22e18110505d4cf53bef0745baf42881d5e109a
Sara A <calisaraalhindi@gmail.com>
2020-11-03 22:00:14 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,5 +1,7 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
 class Puppy 
+  attr_reader :name 
+  
   def initialize(name)
     @name = name
   end
